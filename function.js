f7148bb0f3e16ff2c22e4d87ce9fb78847808117
Sami Suo-Heikki <samiheikki@vaadin.com>
2017-06-02 21:48:31 +0300

    Add real options

--- function.js
+++ function.js
@@ -1,6 +1,87 @@
-const options = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49'];
+const options = [
+    'Sulhasen äiti ei voi lopettaa itkemistä',
+    'Morsian ei voi lopettaa itkemistä',
+    'Morsiamen isä ei voi lopettaa itkemistä',
+    'Morsiamen äiti ei voi lopettaa itkemistä',
+    'Tanssitaan valssi',
+    'Leikataan kakkua',
+    'Morsian kompastuu helmoihinsa',
+    'Sulhanen kompastuu morsiamen helmaan',
+    'Sataa vettä',
+    'Aurinko paistaa',
+    'Puhe naurattaa',
+    'Puhe itkettää',
+    'Joku käy grillillä välipalalla',
+    'Joku käy jokilaivalla välipalalla',
+    'Hääpari suutelee',
+    'Juoma läikkyy',
+    'Kuokkavieraita',
+    'Sulhanen riisuu takin',
+    'Morsiamesta / sulhasesta kerrotaan lapsuusmuisto',
+    'Hääparista kerrotaan muisto',
+    'Sormuksia ihastellaan',
+    'Morsian ryöstetään',
+    'Morsian / anoppi kyynelehtii',
+    'Kaaso / bestman juoksee',
+    'Morsian vaihtaa kengät',
+    'Joku pitää puheen',
+    'Bestman /sulhanen / morsian on hukassa',
+    'Kaaso nappaa kimpun',
+    'Bestman nappaa sukkanauhan',
+    'Puhelin soi',
+    'Tanssilattia täyttyy',
+    'Vessajono',
+    'Sulhanen vaimottelee',
+    'Säätä kommentoidaan',
+    'Piilopullo',
+    'Bestman / Kaaso / sulhanen pitää puheen',
+    'Anoppi joraa',
+    'Karkkibuffet tyhjenee',
+    'Jotain menee rikki',
+    'Tekniikka pettää',
+    'Häävalssi',
+    'Joku huijaa bingossa',
+    'Nolo muisto polttareista',
+    'Selfie',
+    'Joku huutaa BINGO!',
+    'Reikä sukkahousuissa',
+    'Tutustut uuteen ihmiseen',
+    'Isä antaa neuvon',
+    'Hääpari liikuttuu',
+    'Morsian esittelee sormustaan',
+    'Anoppi tanssittaa sulhasta',
+    'Puhe!',
+    'Hääpari leikkaa kakun',
+    'Hääpari yllätetään',
+    'Kohotetaan malja',
+    'Joku tanssii kravatti otsalla',
+    'Joku kompastuu',
+    'Appiukko kertoo vitsin',
+    'Joku etsii vessaa',
+    'Joku kehuu ruokaa',
+    'Yhteiskuva tai ryhmäselfie',
+    'Kaaso auttaa morsianta',
+    'Joku nousee tuolille seisomaan',
+    'Lapset villiintyvät',
+    'Puhelin soi',
+    'Joku kertoo vitsin',
+    'Sulhanen kyynelehtii',
+    'Morsian on hukassa',
+    'Huulipunaa lisätään',
+    'Kimppu heitetään',
+    'Sulhanen santsaa',
+    'Joku kommentoi säätä',
+    'Joku humaltuu',
+    '"Minä muistan kun..."',
+    'Bestman esittää tanssiperformanssin',
+    'Joku syö hääkarkin',
+    'Avioliittoneuvo',
+    'Juhlavieras luopuu korkkareistaan',
+    'Itkettävä puhe',
+    'Sulhanen pyörtyy'
+]; 
+    
 const numberOfBingoCards = 50;
-
 
 const bingos = [];
 for (let i = 0; i < numberOfBingoCards; i++) {
@@ -8,7 +89,7 @@ for (let i = 0; i < numberOfBingoCards; i++) {
   for (let j = 0; j < 5; j++) {
     const currentRow = [];
     while (currentRow.length < 5) {
-      let randomOption = Math.floor(Math.random() * options.length);
+      let randomOption = options[Math.floor(Math.random()*options.length)];
       if (!currentRow.includes(randomOption)) {
         let addOption = true;
         currentBingo.forEach(row => {
